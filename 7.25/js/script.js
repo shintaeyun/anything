f7da60a2b00e4c94c0dfc42eec398b14b8c6ea4e
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-26 22:47:38 +0900

    Add files via upload

---
+++ 7.25/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#gnb>li').hover(function() {
+        $('.submenu', this).stop().slideDown();
+    }, function() {
+        $('.submenu', this).stop().slideUp();
+    });
+
+    var idx = 0;
+    setInterval(function() {
+        $('#slide>a').eq(idx).fadeOut();
+        idx==2?idx=0:idx++;
+        $('#slide>a').eq(idx).fadeIn();
+    }, 3000);
+
+    $('#pop').click(function() {
+        $('#popup').fadeIn();
+    });
+
+    $('#close').click(function() {
+        $('#popup').hide();
+    });
+
+    $('#site').change(function() {
+        open($(this).val());
+    });
+
+});
