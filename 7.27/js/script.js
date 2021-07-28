30d048407da69d8f121cb440b0c57c5d007c7348
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-28 13:40:31 +0900

    Update script.js

--- 7.27/js/script.js
+++ 7.27/js/script.js
@@ -1,1 +1,25 @@
+$(document).ready(function() {
+    $('.bar').click(function() {
+        $('header>nav').animate({left: 0});
+    });
+
+    $('.close').click(function() {
+        $('header>nav').animate({left: '-80%'});
+    });
+
+    $(window).on('touchstart', function(event) {
+        start_x = event.originalEvent.touches[0].pageX;
+    });
+    $(window).on('touchend', function(event) {
+        end_x = event.originalEvent.changedTouches[0].pageX;
+        var dist = end_x - start_x;
+        if(dist>=80) {
+            $('header>nav').animate({left: 0});
+        }
+        if(dist<=-80) {
+            $('header>nav').animate({left: '-80%'});
+        }
+    });
+});
+
 
